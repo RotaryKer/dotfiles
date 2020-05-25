2e7bc70851f7716d968fdc6a7b84026d4af26cb8
Ryouhei Kakizaki <kakky.ryouhei@gmail.com>
2020-05-26 01:52:21 +0900

    test

--- setting.sh
+++ setting.sh
@@ -1,3 +1,7 @@
-mkdir -p ~/.cache/dein
-curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
-sh ./installer.sh ~/.cache/dein
+# set coding environment with neovim
+echo export XDC_CONFIG_HOME=~/dotfiles >> ~/.zshrc
+source ~/.zshrc
+#mkdir -p ~/.cache/dein
+#curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+#sh ./installer.sh ~/.cache/dein
+#cp -r .config ../
